c913637916700ce076c67a8abf02ccf9d0de6f97
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-03-28 12:30:35 +0530

    Added react fragment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Hero } from './container';
 const App = () => {
   return (
-    <div>
-        <Navbar />
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+};
 
-export default App
+export default App;
